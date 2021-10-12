498fffe541ee033d94f9076599e4367096e38aad
Ifycode <marychord@gmail.com>
2021-10-12 14:31:26 +0100

    Load less (library) styles with webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import './css/styles.css';
+//import library (less) styles
+import './less/styles.less';
+
+//import localhost:4040 page styles
+import './css/page-wip.css';
+
+//Test that javascript works
 import { dummyMsg } from '../buildscripts/customize/chalk';
 
 console.log(dummyMsg);
--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,12 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        test: /\.(css|less)$/i,
+        use: [
+          'style-loader', 
+          'css-loader',
+          'less-loader'
+        ]
       }
     ]
   },
