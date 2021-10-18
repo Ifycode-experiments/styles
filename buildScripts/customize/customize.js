71700043ad11e946f1b3cfe2da2349288cda3101
Ifycode <marychord@gmail.com>
2021-10-19 00:06:26 +0100

    Add temporary documentation on page opened at localhost port 4040

--- buildScripts/customize/customize.js
+++ buildScripts/customize/customize.js
@@ -6,7 +6,7 @@ export const portUrl = `http://localhost:${port}`;
 export const openPort = chalk.greenBright('Open:', chalk.blueBright(portUrl));
 
 //startMsg.js
-export const startMsg = chalk.greenBright('Starting', chalk.blueBright('styles-library') +' in dev mode');
+export const startMsg = chalk.greenBright('Starting', chalk.blueBright('collabo styles-library') +' in dev mode');
 
 //index.js
-export const jsTestMsg = 'Testing javascript for styles-library page works!';
+export const jsTestMsg = 'Testing javascript for collabo styles-library page works!';
